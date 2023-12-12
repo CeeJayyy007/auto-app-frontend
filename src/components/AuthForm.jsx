deec29cf237883acf24e436dbb5f09441e1c8a90
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-12 00:38:29 -0500

    feat: add label, form, input, and authForm components

---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+const AuthForm = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>Sign In with Email</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
