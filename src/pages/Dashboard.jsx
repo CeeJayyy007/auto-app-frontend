8f715a8eab19aa962d1f24ea73294ad5baa32958
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-13 18:55:30 -0500

    feat: add dashboard component

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import DashboardCard from '@/components/dashboard/DashboardCard';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <DashboardCard />
+    </div>
+  );
+};
+
+export default Dashboard;
