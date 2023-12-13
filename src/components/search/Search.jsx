b48300ad8d441cc1ba93f2388ea89a5a45ce19e2
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-13 18:54:52 -0500

    feat: add search component

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,17 @@
+import { Input } from '@/components/ui/Input';
+import { Icons } from '@/components/icons';
+
+const Search = () => {
+  return (
+    <div className="flex items-center relative">
+      <Icons.search className="absolute left-2 top-1/2 transform -translate-y-1/2" />
+      <Input
+        type="search"
+        placeholder="Search..."
+        className="md:w-[100px] lg:w-[300px] pl-10 hover:bg-gray-100 dark:hover:bg-zinc-900 transition-colors duration-200"
+      />
+    </div>
+  );
+};
+
+export default Search;
