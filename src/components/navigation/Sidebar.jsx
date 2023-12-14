16b1a09f3ba02a852b3364d75e880ba40dc2ee58
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-14 02:49:36 -0500

    feat: update sidebar to make menu buttons navigate to desired route

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -1,20 +1,32 @@
-import { Button } from '@/components/ui/button';
 import IconButton from '@/components/IconButton';
 import {
   sidebarContent as content,
   bottomNavContent as bottomContent
 } from './navUtils';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    console.log(path);
+    navigate(path);
+  };
+
   return (
     <div className="flex flex-col h-full w-[200px] relative pr-8">
       <div className="space-y-3 py-4">
         {content.map((item) => (
-          <IconButton key={item.title} {...item} variant="ghost" />
+          <IconButton
+            key={item.title}
+            {...item}
+            variant="ghost"
+            onClick={() => handleClick(item.path)}
+          />
         ))}
       </div>
 
-      <div className="absolute space-y-3 inset-x-0 bottom-8">
+      <div className="absolute space-y-3 bottom-8 pr-8">
         {bottomContent.map((item) => (
           <IconButton key={item.title} {...item} variant="ghost" />
         ))}
