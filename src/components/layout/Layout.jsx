610c0a1fcf4a09d1e10fdcf47f0ed4d13e972987
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-14 17:42:08 -0500

    refactor: update components and refactor code

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -6,13 +6,16 @@ import { Suspense } from 'react';
 
 const Layout = () => {
   return (
-    <div className="flex flex-col h-screen md:container md:mx-auto">
+    <div className="flex flex-col h-screen md:container md:mx-auto md:px-0">
       <MainNav />
       <div className="flex flex-row h-screen">
         <Sidebar />
         {/* <Suspense fallback={<CustomLoader />}> */}
         <Suspense>
-          <div className="bg-gray-100 rounded w-screen p-4">
+          <div className="bg-gray-100 rounded-lg w-screen p-4">
+            <h4 className="text-2xl my-2 font-bold tracking-tight">
+              Welcome back, User
+            </h4>
             <Outlet />
           </div>
         </Suspense>
