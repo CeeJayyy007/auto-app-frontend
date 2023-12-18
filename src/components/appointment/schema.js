240358c14114253f71872430a78fb13339b00369
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-18 13:20:08 -0500

    chore: remove appointment schema

--- src/components/appointment/schema.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const appointmentSchema = z.object({
-  id: z.number(),
-  date: z.string(),
-  status: z.string(),
-  note: z.string(),
-  services: z.array(z.string())
-});
