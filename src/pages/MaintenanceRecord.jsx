763add6dd0b293708caca046226c506ac46f233c
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-19 06:41:21 -0500

    feat: add maintenance record component

---
+++ src/pages/MaintenanceRecord.jsx
@@ -0,0 +1,9 @@
+const MaintenaceRecord = () => {
+  return (
+    <div className="flex flex-row justify-between">
+      <h3 className="mb-4 font-bold text-gray-700">Maintenance Record</h3>
+    </div>
+  );
+};
+
+export default MaintenaceRecord;
