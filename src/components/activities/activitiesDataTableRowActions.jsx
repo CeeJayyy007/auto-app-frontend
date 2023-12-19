4a931e60cc73cbc7424ae097c41b515f98396825
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-19 06:42:58 -0500

    feat: add data row action component for activities component

---
+++ src/components/activities/activitiesDataTableRowActions.jsx
@@ -0,0 +1,37 @@
+import { DotsVerticalIcon } from '@radix-ui/react-icons';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { useNavigate } from 'react-router-dom';
+
+export const ActivitiesDataTableRowActions = ({ row }) => {
+  const navigate = useNavigate();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted focus:ring-0 focus-visible:ring-0 rounded-full"
+        >
+          <DotsVerticalIcon className="h-4 w-4" />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[160px]">
+        <DropdownMenuItem onClick={() => navigate('/maintenance-record/1')}>
+          View
+        </DropdownMenuItem>
+        <DropdownMenuItem>Edit</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
