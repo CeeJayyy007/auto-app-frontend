827f38dbf093b13d57e01371e3cc3fb00c3d166c
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-20 09:42:23 -0500

    chore: update icon fill property

--- src/components/navigation/MainNav.jsx
+++ src/components/navigation/MainNav.jsx
@@ -6,7 +6,7 @@ const MainNav = () => {
   return (
     <nav className="flex items-center justify-between h-16 px-4 py-4">
       <h3 className="flex flex-row">
-        <Icons.logo className="h-6 w-auto" /> Auto App
+        <Icons.logo className="h-6 w-auto fill-gray-400" /> Auto App
       </h3>
       <div className="ml-auto flex items-center space-x-4">
         <Search />
