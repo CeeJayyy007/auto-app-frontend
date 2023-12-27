cee27cf07036ccc7fbb58c64435c1f5b8cc29343
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-27 16:40:34 -0500

    fix: add context and hooks to handle authentication

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useReducer, useContext } from 'react';
+
+const initialState = null;
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return action.payload;
+    case 'LOGOUT':
+      return null;
+    default:
+      return state;
+  }
+};
+
+const UserContext = createContext();
+
+export const UserContextProvider = (props) => {
+  const [user, userDispatch] = useReducer(userReducer, initialState);
+
+  return (
+    <UserContext.Provider value={[user, userDispatch]}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserValue = () => {
+  const userAndDispatch = useContext(UserContext);
+  return userAndDispatch[0];
+};
+
+export const useUserDispatch = () => {
+  const userAndDispatch = useContext(UserContext);
+  return userAndDispatch[1];
+};
+
+export default UserContext;
