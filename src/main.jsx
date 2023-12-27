abfa0b0e6476d5351bda2c8867f0f2ead28bdd6b
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-27 18:37:59 -0500

    feat: add toast and toaster to app

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './styles/globals.css';
 import { NotificationContextProvider } from './context/NotificationContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { UserContextProvider } from './context/UserContext';
+import { Toaster } from './components/ui/toaster.jsx';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NotificationContextProvider>
       <UserContextProvider>
         <App />
+        <Toaster />
       </UserContextProvider>
     </NotificationContextProvider>
   </QueryClientProvider>
