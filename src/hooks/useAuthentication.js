25ef738b11b638772335af0ba732e5ce19d69928
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-29 17:14:34 -0500

    fix: update useAuth hook to ser t token for profile service

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,16 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import authService from '../services/auth';
+import profileService from '../services/profile';
 import { useMutation } from '@tanstack/react-query';
-import { useToast } from '@/components/ui/use-toast';
+import useErrorHandler from './useErrorhandler';
 
 const useAuthentication = (dispatchUser, navigate) => {
-  const { toast } = useToast();
+  const { errorHandler } = useErrorHandler();
 
   useEffect(() => {
     const loggedInUserJSON = window.localStorage.getItem('loggedInUser');
     if (loggedInUserJSON) {
       const user = JSON.parse(loggedInUserJSON);
+      profileService.setToken(user.token);
       dispatchUser({ type: 'SET_USER', payload: user });
     }
   }, []);
@@ -19,18 +21,15 @@ const useAuthentication = (dispatchUser, navigate) => {
     mutationFn: authService.login,
     onSuccess: (data) => {
       window.localStorage.setItem('loggedInUser', JSON.stringify(data));
+      console.log('data', data);
+      profileService.setToken(data.token);
       dispatchUser({ type: 'SET_USER', payload: data });
       navigate('/', { replace: true });
     },
     onError: (error) => {
       const message = error?.response?.data?.error || 'Something went wrong';
 
-      toast({
-        title: 'Authentication Error',
-        description: message,
-        variant: 'error',
-        duration: 5000
-      });
+      errorHandler(error, 'Authentication Error', message);
     }
   });
 
@@ -38,18 +37,14 @@ const useAuthentication = (dispatchUser, navigate) => {
     mutationFn: authService.register,
     onSuccess: (data) => {
       window.localStorage.setItem('loggedInUser', JSON.stringify(data));
+      profileService.setToken(data.token);
       dispatchUser({ type: 'SET_USER', payload: data });
       navigate('/', { replace: true });
     },
     onError: (error) => {
       const message = error?.response?.data?.error || 'Something went wrong';
 
-      toast({
-        title: 'Authentication Error',
-        description: message,
-        variant: 'error',
-        duration: 5000
-      });
+      errorHandler(error, 'Authentication Error', message);
     }
   });
 
