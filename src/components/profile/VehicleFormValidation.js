207ad608c54af9b053c9bfc7375abf7521e592b4
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-12-30 18:26:22 -0500

    feat: add vehicle form and validation

---
+++ src/components/profile/VehicleFormValidation.js
@@ -0,0 +1,19 @@
+import * as z from 'zod';
+
+const AddVehicleFormSchema = z.object({
+  make: z.string().min(2, 'Must be at least 2 characters'),
+  model: z.string().min(2, 'Must be at least 2 characters'),
+  year: z.string().min(4, 'Must be at least 4 characters'),
+  registrationNumber: z.string().min(2, 'Must be at least 2 characters')
+});
+
+const EditVehicleFormSchema = z
+  .object({
+    make: z.string().min(2, 'Must be at least 2 characters'),
+    model: z.string().min(2, 'Must be at least 2 characters'),
+    year: z.string().min(4, 'Must be at least 4 characters'),
+    registrationNumber: z.string().min(2, 'Must be at least 2 characters')
+  })
+  .optional();
+
+export { AddVehicleFormSchema, EditVehicleFormSchema };
