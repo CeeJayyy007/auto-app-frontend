3287627a54b324eb4e53005202751053c7c46c76
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-02 12:49:12 -0500

    feat: add successhandler and update error handler

--- src/hooks/useErrorHandler.js
+++ src/hooks/useErrorHandler.js
@@ -1,24 +1,75 @@
 import { useToast } from '@/components/ui/use-toast';
-import { useUserDispatch } from '@/context/UserContext';
+import codeMessage from '../utils/codeMessage';
+// import { useUserDispatch } from '@/context/UserContext';
 
 const useErrorHandler = () => {
-  const dispatchUser = useUserDispatch();
+  //   const dispatchUser = useUserDispatch();
   const { toast } = useToast();
 
-  const errorHandler = (error, title, message) => {
-    const getToast = toast({
-      title: title,
-      description: message,
-      variant: 'error',
-      duration: 5000
-    });
+  const errorHandler = (error, title) => {
+    // if (error.includes('jwt expired')) {
+    //   window.localStorage.removeItem('loggedInUser');
+    //   dispatchUser({ type: 'LOGOUT' });
+    //   getToast();
+    // } else {
+    //   getToast();
+    // }
+    // };
 
-    if (error.includes('jwt expired')) {
-      window.localStorage.removeItem('loggedInUser');
-      dispatchUser({ type: 'LOGOUT' });
-      getToast();
+    const { response } = error;
+
+    if (response.data && response.data.jwtExpired) {
+      const result = window.localStorage.getItem('auth');
+      const jsonFile = window.localStorage.getItem('isLogout');
+      const { isLogout } = (jsonFile && JSON.parse(jsonFile)) || false;
+      window.localStorage.removeItem('auth');
+      window.localStorage.removeItem('isLogout');
+      if (result || isLogout) {
+        window.location.href = '/logout';
+      }
+    }
+
+    if (response && response.status) {
+      const message = response.data && response.data.message;
+
+      const errorText = message || codeMessage[response.status];
+      // const { status } = response;
+      toast({
+        title: title,
+        description: errorText,
+        variant: 'error',
+        duration: 5000
+      });
+
+      return response.data;
     } else {
-      getToast();
+      if (navigator.onLine) {
+        // Code to execute when there is internet connection
+        toast({
+          title: 'Problem connecting to server',
+          description: 'Cannot connect to the server, Try again later',
+          variant: 'error',
+          duration: 5000
+        });
+        return {
+          success: false,
+          result: null,
+          message: 'Cannot connect to the server, Check your internet network'
+        };
+      } else {
+        // Code to execute when there is no internet connection
+        toast({
+          title: 'Problem connecting to server',
+          description: 'Cannot connect to the server, Try again later',
+          variant: 'error',
+          duration: 5000
+        });
+        return {
+          success: false,
+          result: null,
+          message: 'Cannot connect to the server, Check your internet network'
+        };
+      }
     }
   };
 
