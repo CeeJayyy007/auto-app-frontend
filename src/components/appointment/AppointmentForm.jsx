dd973e2957395be715c48cb2bce6feed15d3ce55
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-02 19:33:40 -0500

    feat: add appoitment form and validation

---
+++ src/components/appointment/AppointmentForm.jsx
@@ -0,0 +1,144 @@
+import { useForm } from 'react-hook-form';
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SheetClose } from '../ui/sheet';
+
+const AppointmentForm = ({
+  appointment,
+  formAction,
+  formValidation,
+  buttonText,
+  props
+}) => {
+  const form = useForm({
+    resolver: zodResolver(formValidation),
+    defaultValues: {
+      make: appointment ? appointment.make : '',
+      model: appointment ? appointment.model : '',
+      year: appointment ? appointment.year : '',
+      registrationNumber: appointment ? appointment.registrationNumber : ''
+    }
+  });
+
+  console.log(form.formState.errors);
+
+  const onSubmit = async (data) => {
+    try {
+      if (appointment) {
+        await formAction(data, appointment.id);
+      } else {
+        await formAction(data);
+      }
+
+      form.reset();
+    } catch (error) {
+      form.setError('submitError', {
+        type: 'manual',
+        message: 'Submission failed. Please try again.'
+      });
+    }
+  };
+
+  return (
+    <div className="grid gap-6" {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="make"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Make</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Make"
+                      disabled={form.formState.isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  {form.formState.errors.make && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="model"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Model</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Model"
+                      disabled={form.formState.isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  {form.formState.errors.model && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="year"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Year</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Year"
+                      disabled={form.formState.isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  {form.formState.errors.year && <FormMessage />}
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="registrationNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Registration Number</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Registration Number"
+                      disabled={form.formState.isLoading}
+                      {...field}
+                    />
+                  </FormControl>
+                  {form.formState.errors.registrationNumber && <FormMessage />}
+                </FormItem>
+              )}
+            />
+            {/* <SheetClose asChild> */}
+            <Button
+              className="mt-8"
+              disabled={form.formState.isLoading}
+              type="submit"
+            >
+              {buttonText}
+            </Button>
+            {/* </SheetClose> */}
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default AppointmentForm;
--- src/components/appointment/AppointmentValidation.js
+++ src/components/appointment/AppointmentValidation.js
@@ -1,11 +1,27 @@
 import * as z from 'zod';
 
-const appointmentSchema = z.object({
-  date: z.string(),
-  time: z.string(),
-  vehicle: z.string(),
-  services: z.array(z.string()),
-  note: z.string()
+const AddAppointmentSchema = z.object({
+  date: z.string().min(10, 'Must be at least 10 characters'),
+  time: z.string().min(5, 'Must be at least 5 characters'),
+  vehicle: z.array(z.string()).min(1, 'Must select at least 1 vehicle'),
+  services: z.array(z.string()).min(1, 'Must select at least 1 service'),
+  note: z
+    .string()
+    .min(2, 'Must be at least 2 characters')
+    .max(500, 'Must be less than 500 characters')
 });
 
-export default appointmentSchema;
+const EditAppointmentSchema = z
+  .object({
+    date: z.string().min(10, 'Must be at least 10 characters'),
+    time: z.string().min(5, 'Must be at least 5 characters'),
+    vehicle: z.array(z.string()).min(1, 'Must select at least 1 vehicle'),
+    services: z.array(z.string()).min(1, 'Must select at least 1 service'),
+    note: z
+      .string()
+      .min(2, 'Must be at least 2 characters')
+      .max(500, 'Must be less than 500 characters')
+  })
+  .partials();
+
+export default { AddAppointmentSchema, EditAppointmentSchema };
