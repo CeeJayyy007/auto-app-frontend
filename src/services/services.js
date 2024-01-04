e56518306680ff9c653f6b0863ff01366a12fba8
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-04 13:12:45 -0500

    chore: add services service

---
+++ src/services/services.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { token } from '@/utils/auth';
+const baseUrl = '/api/users';
+const servicesUrl = '/api/services';
+
+const getAll = async (id) => {
+  const response = await axios.get(`${baseUrl}`);
+  return response.data;
+};
+
+const getServiceById = async (id) => {
+  const response = await axios.get(`${servicesUrl}/${id}`);
+  return response.data;
+};
+
+const addService = async (newObject, id) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.post(
+    `${baseUrl}/${id}/add-service`,
+    newObject,
+    config
+  );
+  return response.data;
+};
+
+const updateService = async (newObject, id) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.put(`${servicesUrl}/${id}`, newObject, config);
+  return response.data;
+};
+
+const removeService = async (id) => {
+  const config = {
+    headers: { Authorization: token }
+  };
+
+  const response = await axios.delete(`${servicesUrl}/${id}`, config);
+  return response.data;
+};
+
+export default {
+  getAll,
+  getServiceById,
+  addService,
+  updateService,
+  removeService
+};
