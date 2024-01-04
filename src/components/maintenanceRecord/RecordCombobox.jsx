2863a0f1c07bf9e7f2177c2f10ef7b9e9edbbfaf
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-04 13:08:10 -0500

    fix: add form functions to record combobox

--- src/components/maintenanceRecord/RecordCombobox.jsx
+++ src/components/maintenanceRecord/RecordCombobox.jsx
@@ -17,8 +17,15 @@ import {
 import { cn } from '@/lib/utils';
 import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 import ItemCard from './ItemCard';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '../ui/form';
 
-const RecordCombobox = ({ data, name }) => {
+const RecordCombobox = ({ data, name, form, label, formName }) => {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState({});
 
@@ -32,10 +39,16 @@ const RecordCombobox = ({ data, name }) => {
         updatedSelected[value] = label;
       }
 
+      const serviceId = Object.values(updatedSelected)
+        .map((serviceName) =>
+          data.find((service) => service.value === serviceName)
+        )
+        .filter((matchedService) => matchedService)
+        .map((matchedService) => matchedService.id);
+
+      form.setValue(formName, serviceId);
       return updatedSelected;
     });
-
-    setOpen(false);
   };
 
   const lastSelectedLabel =
@@ -46,60 +59,84 @@ const RecordCombobox = ({ data, name }) => {
       : `Add ${name}...`;
 
   return (
-    <div>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[200px] justify-between mb-2"
-          >
-            {lastSelectedLabel}
-            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder={`Search ${name}...`} className="h-9" />
-            <CommandEmpty>No item found.</CommandEmpty>
-            <CommandGroup>
-              {data.map((item) => (
-                <CommandItem
-                  key={item.value}
-                  value={item.value}
-                  onSelect={() => handleSelect(item)}
+    <FormField
+      control={form.control}
+      name={formName}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel className="sr-only">{label}</FormLabel>
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className={cn(
+                    'w-full justify-between mb-2',
+                    !field.value && 'text-muted-foreground'
+                  )}
                 >
-                  {item.label}
-                  <CheckIcon
-                    className={cn(
-                      'ml-auto h-4 w-4',
-                      selected[item.value] === item.value
-                        ? 'opacity-100'
-                        : 'opacity-0'
-                    )}
-                  />
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      <div className="grid gap-4 h-[130px] border rounded-lg p-2">
-        {Object.keys(selected).length > 0 ? (
-          <ScrollArea className=" max-h-[400px]">
-            {Object.entries(selected).map(([key, value]) => (
-              <ItemCard key={key} name={value} />
-            ))}
-            <ScrollBar orientation="vertical" />
-          </ScrollArea>
-        ) : (
-          <p className="text-sm text-muted-foreground text-center col-span-3 grid items-center">
-            {`No ${name} selected`}
-          </p>
-        )}
-      </div>
-    </div>
+                  {lastSelectedLabel}
+                  <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0 m-2" align="end">
+              <Command>
+                <CommandInput
+                  placeholder={`Search ${name}...`}
+                  className="h-9"
+                />
+                <CommandEmpty>No item found.</CommandEmpty>
+                <CommandGroup>
+                  {data.map((item) => (
+                    <CommandItem
+                      key={item.value}
+                      value={item.value}
+                      onSelect={() => handleSelect(item)}
+                    >
+                      {item.label}
+                      <CheckIcon
+                        className={cn(
+                          'ml-auto h-4 w-4',
+                          selected[item.value] === item.value
+                            ? 'opacity-100'
+                            : 'opacity-0'
+                        )}
+                      />
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </Command>
+              <Button
+                variant="outline"
+                className="text-center w-[180px] my-2 ml-2"
+                onClick={() => setOpen(false)}
+              >
+                Close
+              </Button>
+            </PopoverContent>
+          </Popover>
+          <div className="grid gap-4 h-[130px] border rounded-lg p-2">
+            {Object.keys(selected).length > 0 ? (
+              <ScrollArea className=" max-h-[400px]">
+                {Object.entries(selected).map(([key, value]) => (
+                  <ItemCard key={key} name={value} />
+                ))}
+                <ScrollBar orientation="vertical" />
+              </ScrollArea>
+            ) : (
+              <p className="text-sm text-muted-foreground text-center col-span-3 grid items-center">
+                {`No ${name} selected`}
+              </p>
+            )}
+          </div>
+
+          {form.formState.errors[formName] && <FormMessage />}
+        </FormItem>
+      )}
+    />
   );
 };
 
