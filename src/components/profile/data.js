faa3dc3b50a8e431f12202153d1a035700c01082
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-10 01:41:08 -0500

    chore: update data

---
+++ src/components/profile/data.js
@@ -0,0 +1,14 @@
+export const statuses = [
+  {
+    value: 'User',
+    label: 'User'
+  },
+  {
+    value: 'Admin',
+    label: 'Admin'
+  },
+  {
+    value: 'Super Admin',
+    label: 'Super Admin'
+  }
+];
