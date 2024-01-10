5d473a746cdb4fd46d458f00ecf8ee1dda8b9016
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-10 01:43:23 -0500

    feat: add user to nav

--- src/components/navigation/navUtils.jsx
+++ src/components/navigation/navUtils.jsx
@@ -9,48 +9,54 @@ export const sidebarContent = [
   },
   {
     id: 2,
+    title: 'Users',
+    icon: getIcon({ name: 'profile', className: 'mr-2' }),
+    path: '/users'
+  },
+  {
+    id: 3,
     title: 'Profile',
     icon: getIcon({ name: 'profile', className: 'mr-2' }),
     path: '/profile'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Appointments',
     icon: getIcon({ name: 'appointments', className: 'mr-2 ' }),
     path: '/appointments'
   },
   {
-    id: 4,
+    id: 5,
     title: 'Activities',
     icon: getIcon({ name: 'activities', className: 'mr-2' }),
     path: '/activities'
   },
   {
-    id: 5,
+    id: 6,
     title: 'Services',
     icon: getIcon({ name: 'services', className: 'mr-2' }),
     path: '/services'
   },
   {
-    id: 6,
+    id: 7,
     title: 'Inventory',
     icon: getIcon({ name: 'cart', className: 'mr-2' }),
     path: '/inventory'
   },
   {
-    id: 7,
+    id: 8,
     title: 'Payments',
     icon: getIcon({ name: 'payments', className: 'mr-2' }),
     path: '/payments'
   },
   {
-    id: 8,
+    id: 9,
     title: 'Reports',
     icon: getIcon({ name: 'reports', className: 'mr-2' }),
     path: '/reports'
   },
   {
-    id: 9,
+    id: 10,
     title: 'Analytics',
     icon: getIcon({ name: 'chart', className: 'mr-2' }),
     path: '/analytics'
