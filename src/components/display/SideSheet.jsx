9c81b3ee8a25c4c01ee58efbc6fbafc9195ba77f
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-15 04:14:04 -0500

    fix: add classname prop

--- src/components/display/SideSheet.jsx
+++ src/components/display/SideSheet.jsx
@@ -7,18 +7,29 @@ import {
   SheetTitle,
   SheetTrigger
 } from '@/components/ui/sheet';
+import { cn } from '@/lib/utils';
 
-const SideSheet = ({ body, triggerLabel, title, description, type }) => {
+const SideSheet = ({
+  body,
+  triggerLabel,
+  title,
+  description,
+  type,
+  className
+}) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
         {type === 'button' ? (
-          <Button>{triggerLabel}</Button>
+          <Button className={cn(className)}>{triggerLabel}</Button>
         ) : (
           <Button
             role="menuitem"
             variant="outline border-none"
-            className="w-full flex cursor-default font-normal justify-start select-none items-center hover:bg-gray-100 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+            className={cn(
+              'w-full flex cursor-default font-normal justify-start select-none items-center hover:bg-gray-100 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+              className
+            )}
           >
             {triggerLabel}
           </Button>
