5af7753926660486633a2589fa93e40cf1bc9eba
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-19 00:29:01 -0500

    fix: add reducers to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import vehicleReducer from '../reducers/vehicleReducers';
 import appointmentReducer from '../reducers/appointmentReducers';
 import activitiesReducers from '@/reducers/activitiesReducers';
+import serviceReducers from '@/reducers/serviceReducers';
+import inventoryReducers from '@/reducers/inventoryReducers';
+import profileReducers from '@/reducers/profileReducers';
 
 const store = configureStore({
   reducer: {
     vehicle: vehicleReducer,
     appointment: appointmentReducer,
-    activities: activitiesReducers
+    activities: activitiesReducers,
+    service: serviceReducers,
+    inventory: inventoryReducers,
+    profile: profileReducers
   }
 });
 
