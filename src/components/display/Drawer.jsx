a4a17513be13154449bcbc03e033739f89e8f944
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-01-20 01:15:48 -0500

    fix: update drawer component

--- src/components/display/Drawer.jsx
+++ src/components/display/Drawer.jsx
@@ -18,18 +18,24 @@ const DrawerComponent = ({
   actionLabel,
   body,
   type,
-  onClick
+  onClick,
+  clickable,
+  content
 }) => {
   return (
     <Drawer>
       <DrawerTrigger asChild>
-        <Button
-          role="menuitem"
-          variant="outline border-none"
-          className="w-full relative cursor-default font-normal justify-start select-none items-center hover:bg-gray-100 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
-        >
-          {triggerLabel}
-        </Button>
+        {clickable ? (
+          content
+        ) : (
+          <Button
+            role="menuitem"
+            variant="outline border-none"
+            className="w-full relative cursor-default font-normal justify-start select-none items-center hover:bg-gray-100 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+          >
+            {triggerLabel}
+          </Button>
+        )}
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
